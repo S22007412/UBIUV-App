c1c4faf6274f4930578d49b4df2e0748cdf82532
ZS22007412 <zs22007412@estudiantes.uv.mx>
2024-06-02 13:09:15 -0600

    Added locations permissions

--- app/src/main/java/com/uv/ubiuv/MainActivity.java
+++ app/src/main/java/com/uv/ubiuv/MainActivity.java
@@ -1,20 +1,29 @@
 package com.uv.ubiuv;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private WebView mywebView;
+    private boolean locationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +36,55 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        mywebView=(WebView) findViewById(R.id.webview);
-        mywebView.setWebViewClient(new WebViewClient());
-        mywebView.loadUrl("http://ubiuv.duckdns.org/");
-        WebSettings webSettings=mywebView.getSettings();
+        mywebView = findViewById(R.id.webview);
+        mywebView.setWebViewClient(new mywebClient());
+        mywebView.loadUrl("https://ubiuv.duckdns.org/");
+        WebSettings webSettings = mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        mywebView.addJavascriptInterface(new WebAppInterface(), "Android");
+
+        getLocationPermission();
     }
 
-    public class mywebClient extends WebViewClient {
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view,url,favicon);
-        }
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+            updateLocationUI();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+                updateLocationUI();
+            }
+        }
+    }
+
+    private void updateLocationUI() {
+        if (locationPermissionGranted) {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                mywebView.post(() -> mywebView.loadUrl("javascript:updateLocation(" + latitude + "," + longitude + ")"));
+            }
+        }
+    }
+
     @Override
     public void onBackPressed() {
         if (mywebView.canGoBack()) {
@@ -54,4 +94,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public class WebAppInterface {
+        @JavascriptInterface
+        public void getLocation() {
+            if (locationPermissionGranted) {
+                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    mywebView.post(() -> mywebView.loadUrl("javascript:updateLocation(" + latitude + "," + longitude + ")"));
+                }
+            }
+        }
+    }
 }
